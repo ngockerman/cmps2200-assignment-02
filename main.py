7207267142493f1697bd070d8b64c771ea0fbb73
Zhengming Ding <allanzmding@gmail.com>
2022-02-22 16:15:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,33 @@ class BinaryNumber:
 ## Implement multiplication functions here. Note that you will have to
 ## ensure that x, y are appropriately sized binary vectors for a
 ## divide and conquer approach.
+def binary2int(binary_vec): 
+    if len(binary_vec) == 0:
+        return BinaryNumber(0)
+    return BinaryNumber(int(''.join(binary_vec), 2))
+
+def split_number(vec):
+    return (binary2int(vec[:len(vec)//2]),
+            binary2int(vec[len(vec)//2:]))
+
+def bit_shift(number, n):
+    # append n 0s to this number's binary string
+    return binary2int(number.binary_vec + ['0'] * n)
+    
+def pad(x,y):
+    # pad with leading 0 if x/y have different number of bits
+    # e.g., [1,0] vs [1]
+    if len(x) < len(y):
+        x = ['0'] * (len(y)-len(x)) + x
+    elif len(y) < len(x):
+        y = ['0'] * (len(x)-len(y)) + y
+    # pad with leading 0 if not even number of bits
+    if len(x) % 2 != 0:
+        x = ['0'] + x
+        y = ['0'] + y
+    return x,y
+
+
 
 def subquadratic_multiply(x, y):
     ### TODO
