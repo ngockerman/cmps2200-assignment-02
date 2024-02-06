49a1696903050ca738d6895137334a67d0377f86
Zhengming Ding <allanzmding@gmail.com>
2024-02-06 09:16:50 -0600

    Delete convert.sh

--- convert.sh
+++
@@ -1,2 +0,0 @@
-# If you're feeling ambitious, you can convert from Markdown to PDF. You'll need to install pandoc (https://pandoc.org/) and latex. Then, run
-pandoc -V geometry:margin=1in assignment-02.md -o assignment-02.pdf
