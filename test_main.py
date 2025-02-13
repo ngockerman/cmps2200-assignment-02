c409c3cce6c8ee941921ac4f404aa634afa53c56
Zhengming Ding <allanzmding@gmail.com>
2025-02-13 08:44:11 -0600

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,6 +1,29 @@
 from main.py import *
 
-## Feel free to add your own tests here.
+#### Iterative solution test
+def test_parens_match_iterative():
+    assert parens_match_iterative(['(', ')']) == True
+    assert parens_match_iterative(['(']) == False
+    assert parens_match_iterative([')']) == False
+    assert parens_match_iterative(['(', 'a', ')', '(', ')']) == True
+    assert parens_match_iterative(['(',  '(', '(', ')', ')', ')']) == True
+    assert parens_match_iterative(['(', '(', ')']) == False
+    assert parens_match_iterative(['(', 'a', ')', ')', '(']) == False
+    assert parens_match_iterative([]) == True
+
+#### Scan solution test
+def test_parens_match_scan():
+    assert parens_match_scan(['(', ')']) == True
+    assert parens_match_scan(['(']) == False
+    assert parens_match_scan([')']) == False
+    assert parens_match_scan(['(', 'a', ')', '(', ')']) == True
+    assert parens_match_scan(['(',  '(', '(', ')', ')', ')']) == True
+    assert parens_match_scan(['(', '(', ')']) == False
+    assert parens_match_scan(['(', 'a', ')', ')', '(']) == False
+    assert parens_match_scan([]) == True
+
+
+#### Divide and conquer solution test
 def test_parens_match_dc():
     assert parens_match_dc(['(', ')']) == True
     assert parens_match_dc(['(']) == False
